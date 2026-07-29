ea0152fcfb4de1d837862543fd168d88de5d02e1
Maria Eduarda <mariaaraujo4830@gmail.com>
2026-07-29 20:29:33 -0300

    Sistema RH

--- playwright.config.js
+++ playwright.config.js
@@ -11,6 +11,7 @@ module.exports = defineConfig({
     use: {
         baseURL: 'http://127.0.0.1:4173',
         trace: 'retain-on-failure',
+        serviceWorkers: 'block',
     },
     webServer: {
         command: 'node test-support/static-server.js',
